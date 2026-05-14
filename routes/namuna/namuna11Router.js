befd7adadd18874d8a1ffb10f81c9bec95546102
sojwaluttirnaservice <sojwaluttirnaservice@gmail.com>
2026-05-14 12:57:06 +0530

    namuna11Router code refactoring

--- routes/namuna/namuna11Router.js
+++ routes/namuna/namuna11Router.js
@@ -1,37 +1,36 @@
-const { Router } = require('express');
 const { checkForPoolConnection } = require('../middleware');
 const namuna11Controller = require('../../application/controllers/namuna/namuna11Controller');
+const getRouter = require('../../application/utils/getRouter');
 
-const namuna11Router = Router();
+const namuna11Router = getRouter(); 
 
 // Rendering pages
-namuna11Router.get('/', checkForPoolConnection, namuna11Controller.renderNamuna11Page);
+namuna11Router.get('/', namuna11Controller.renderNamuna11Page);
 
-namuna11Router.get('/create', checkForPoolConnection, namuna11Controller.renderNamuna11CreatePage);
+namuna11Router.get('/create', namuna11Controller.renderNamuna11CreatePage);
 
 // Edit
-namuna11Router.get('/edit/:id', checkForPoolConnection, namuna11Controller.renderEditNamuna11Page);
+namuna11Router.get('/edit/:id', namuna11Controller.renderEditNamuna11Page);
 
 // Route to render the report page
-// namuna11Router.get('/report', checkForPoolConnection, namuna11Controller.render);
+// namuna11Router.get('/report', namuna11Controller.render);
 
 // Route to render the print page
-namuna11Router.get('/print', checkForPoolConnection, namuna11Controller.renderNamuna11Print);
+namuna11Router.get('/print', namuna11Controller.renderNamuna11Print);
 
 // CRUD OPTIONS
 // Route to insert a new entry
-namuna11Router.post('/save', checkForPoolConnection, namuna11Controller.saveNamuna11Details);
+namuna11Router.post('/save', namuna11Controller.saveNamuna11Details);
 
 // Route to update a specific entry by ID
-namuna11Router.put('/update', checkForPoolConnection, namuna11Controller.updateNamuna11Details);
+namuna11Router.put('/update', namuna11Controller.updateNamuna11Details);
 
 // Route to delete a specific entry by ID
-namuna11Router.delete('/delete', checkForPoolConnection, namuna11Controller.deleteNamuna11Details);
+namuna11Router.delete('/delete', namuna11Controller.deleteNamuna11Details);
 
 // Route to fetch entries by month and year
 namuna11Router.get(
     '/list',
-    checkForPoolConnection,
     namuna11Controller.fetchAllNamuna11DetailsByMonthAndYear
 );
 
