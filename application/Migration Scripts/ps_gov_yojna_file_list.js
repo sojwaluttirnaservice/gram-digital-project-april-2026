6dcb51543b34cd505ee8274ae923060663b06fc1
sojwaluttirnaservice <sojwaluttirnaservice@gmail.com>
2026-05-14 23:57:52 +0530

    ps_gov_yojna_file_list schema modified     15_May_2026_09_10_Sojwal     - Added addional fiels like yojana name, desc, banner image others

--- application/Migration Scripts/ps_gov_yojna_file_list.js
+++ application/Migration Scripts/ps_gov_yojna_file_list.js
@@ -10,14 +10,72 @@ const ps_gov_yojna_file_list = sequelize.define(
 			primaryKey: true,
 			autoIncrement: true,
 		},
+
+        yojana_name: {
+            type: Sequelize.STRING(200),
+            allowNull: false,
+            defaultValue: ''
+        },
+
+        website_link: {
+            type: Sequelize.STRING(300),
+            allowNull: true,
+        },
+
+        yojana_description: {
+            type: Sequelize.STRING(400),
+            allowNull: true,
+        },
+
+        required_documents_list: {
+            type: Sequelize.JSON,
+            allowNull: true
+        },
+
+        yojana_status: {
+            type: Sequelize.ENUM("ONGOING", "COMPLETE", "UPCOMING"),
+            allowNull: false,
+            defaultValue: "ONGOING"
+        },
+
+        start_date: {
+            type: Sequelize.DATEONLY,
+            allowNull: true,
+        },
+
+        // image name for this
+        image_banner: {
+            type: Sequelize.STRING(200),
+            allowNull: true
+        },
+
+        is_visible: {
+            // 1 => visible, 0 => not visible
+            type: Sequelize.TINYINT,
+            defaultValue: 1
+        },
+
+        // this is the original field from the start
 		file_name: {
 			type: Sequelize.STRING(255),
 			allowNull: false,
+            defaultValue: ''
 		},
+
+        createdAt: {
+            type: Sequelize.DATE,
+            allowNull: true,
+            defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+
+        updatedAt: {
+            type: Sequelize.DATE,
+            allowNull: true,
+            defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        }
 	},
 	{
-		createdAt: false,
-		updatedAt: false,
+		timestamps: true
 	}
 )
 
