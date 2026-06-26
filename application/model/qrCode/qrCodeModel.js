cfc2d8d9ee7655557e625b1aef3edb0e61d1f09f
sojwaluttirnaservice <sojwaluttirnaservice@gmail.com>
2026-06-26 16:06:03 +0530

    feat: add water bank QR code to Form 9 water tax print layout and clean up template indentation 26_June_2026_16_05_Sojwal
    - Integrate `bank_qr_code_water_image_name` image rendering in `pageFormNightPani.pug` print layout.
    - Fix table column alignment and nesting indentation of total cells in `pageFormNightPani.pug`.
    - Format and organize inline CSS style declarations in `pageFormNightPani.pug`.
    - Remove unnecessary border padding in QR code display block of `pageFormNineSamanyaOther.pug`.
    - Apply consistent code formatting and indentation style to `FromPrintController.js`.

--- application/model/qrCode/qrCodeModel.js
+++ application/model/qrCode/qrCodeModel.js
@@ -1,63 +1,35 @@
+const { runQuery } = require('../../utils/runQuery');
+
 const qrCodeModel = {
-  qrCodeList: function (pool) {
-    return new Promise(function (resolve, reject) {
-      const query = "SELECT * FROM ps_qr_codes";
-      pool.query(query, function (error, results) {
-        if (error) reject(error);
-        else resolve(results);
-      });
-    });
-  },
+	qrCodeList: function (pool) {
+		const q = 'SELECT * FROM ps_qr_codes';
+		return runQuery(pool, q);
+	},
 
-  createEntry: function (pool) {
-    return new Promise(function (resolve, reject) {
-      const query = "INSERT INTO ps_qr_codes (id) VALUES (DEFAULT)";
-      pool.query(query, function (error, result) {
-        if (error) reject(error);
-        else resolve(result);
-      });
-    });
-  },
+	createEntry: function (pool) {
+		const q = 'INSERT INTO ps_qr_codes (id) VALUES (DEFAULT)';
+		return runQuery(pool, q)
+	},
 
-  updateBankQRCodeImageName: function (pool, imageName) {
-    return new Promise(function (resolve, reject) {
-      const query = `UPDATE ps_qr_codes SET bank_qr_code_image_name = ?  WHERE id =?`;
-      pool.query(query, [imageName, 1], function (error, result) {
-        if (error) reject(error);
-        else resolve(result);
-      });
-    });
-  },
+	updateBankQRCodeImageName: function (pool, imageName) {
+		const q = `UPDATE ps_qr_codes SET bank_qr_code_image_name = ?  WHERE id =?`;
+		return runQuery(pool, q, [imageName, 1])
+	},
 
-  toggleQrBankCodeVisbility: function (pool, visibilityToSet) {
-    return new Promise(function (resolve, reject) {
-      const query = `UPDATE ps_qr_codes SET show_bank_qr_code_image = ?  WHERE id =?`;
-      pool.query(query, [visibilityToSet, 1], function (error, result) {
-        if (error) reject(error);
-        else resolve(result);
-      });
-    });
-  },
+	toggleQrBankCodeVisbility: function (pool, visibilityToSet) {
+		const q = `UPDATE ps_qr_codes SET show_bank_qr_code_image = ?  WHERE id =?`;
+		return runQuery(pool, q, [visibilityToSet, 1]);
+	},
 
-  updateBankQRCodeWaterImageName: function (pool, imageName) {
-    return new Promise(function (resolve, reject) {
-      const query = `UPDATE ps_qr_codes SET bank_qr_code_water_image_name = ?  WHERE id = ?`;
-      pool.query(query, [imageName, 1], function (error, result) {
-        if (error) reject(error);
-        else resolve(result);
-      });
-    });
-  },
+	updateBankQRCodeWaterImageName: function (pool, imageName) {
+		const q = `UPDATE ps_qr_codes SET bank_qr_code_water_image_name = ?  WHERE id = ?`;
+		return runQuery(pool, q, [imageName, 1]);
+	},
 
-  toggleQrBankCodeWaterVisbility: function (pool, visibilityToSet) {
-    return new Promise(function (resolve, reject) {
-      const query = `UPDATE ps_qr_codes SET show_bank_qr_code_water_image = ?  WHERE id = ?`;
-      pool.query(query, [visibilityToSet, 1], function (error, result) {
-        if (error) reject(error);
-        else resolve(result);
-      });
-    });
-  },
+	toggleQrBankCodeWaterVisbility: function (pool, visibilityToSet) {
+		const q = `UPDATE ps_qr_codes SET show_bank_qr_code_water_image = ?  WHERE id = ?`;
+		return runQuery(pool, q, [visibilityToSet, 1])
+	}
 };
 
 module.exports = qrCodeModel;
